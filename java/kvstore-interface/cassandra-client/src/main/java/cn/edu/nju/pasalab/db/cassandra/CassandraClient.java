85524d18d3a5d8e08fd8aecb5ac488771b2f9dc8
ZK Wang <2674020+wangzk@users.noreply.github.com>
2019-05-26 19:20:07 +0800

    Update the cassandra client implementation.

--- java/kvstore-interface/cassandra-client/src/main/java/cn/edu/nju/pasalab/db/cassandra/CassandraClient.java
+++ java/kvstore-interface/cassandra-client/src/main/java/cn/edu/nju/pasalab/db/cassandra/CassandraClient.java
@@ -1,14 +1,14 @@
 package cn.edu.nju.pasalab.db.cassandra;
 
 import cn.edu.nju.pasalab.db.BasicKVDatabaseClient;
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
-import com.datastax.driver.core.SimpleStatement;
+import com.datastax.driver.core.*;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
+import java.nio.ByteBuffer;
+import java.util.*;
+import java.util.concurrent.Future;
+import java.util.concurrent.Semaphore;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
@@ -19,13 +19,15 @@ public class CassandraClient extends BasicKVDatabaseClient {
     public static final String CONF_TABLE_NAME = "cassandra.table.name";
     public static final String DEFAULT_TABLE_NAME = "datatable";
     public static final String CONF_COLUMN_KEY = "cassandra.column.key";
-    public static final String DEFAULT_COLUMN_KEY = "key";
+    public static final String DEFAULT_COLUMN_KEY = "mykey";
     public static final String CONF_COLUMN_VALUE = "cassandra.column.value";
-    public static final String DEFAULT_COLUMN_VALUE = "value";
+    public static final String DEFAULT_COLUMN_VALUE = "myvalue";
     public static final String CONF_USE_HASHED_KEY = "cassandra.use.hashed.key";
     public static final String DEFAULT_USE_HASHED_KEY = "false";
     public static final String CONF_CONTACT_POINTS = "cassandra.contact.points";
     public static final String DEFAULT_CONTACT_POINTS = "127.0.0.1";
+    public static final String CONF_CONCURRENT_QUERY = "cassandra.concurrent.query";
+    public static final String DEFAULT_CONCURRENT_QUERY = "1000";
 
 
     private List<String> contactPoints;
@@ -35,25 +37,82 @@ public class CassandraClient extends BasicKVDatabaseClient {
     private String keyColumnName;
     private String valueColumnName;
     private boolean useHashedKey;
+    private int numConcurrentQuery;
 
     private Cluster cluster;
     private Session session;
+    private PreparedStatement putStatement;
+    private PreparedStatement getStatement;
+    private Semaphore querySemaphore;
+    private FutureSemaphoreCallback callback;
 
     private Logger logger = Logger.getLogger(this.getClass().getName());
 
     @Override
     public byte[] get(byte[] key) throws Exception {
-        return new byte[0];
+       if (key == null)
+            return null;
+        ByteBuffer keyBuffer = ByteBuffer.wrap(key);
+        BoundStatement statement = getStatement.bind(keyBuffer);
+        ResultSet resultSet = session.execute(statement);
+        Row row = resultSet.one();
+        if (row == null || row.isNull(0))
+            return null;
+        ByteBuffer buf = row.getBytes(0);
+        return buf.array();
+    }
+
+    @Override
+    public byte[][] getAll(byte[][] keys) throws Exception {
+       byte[][] result = new byte[keys.length][];
+        List<ResultSetFuture> futures = new ArrayList<>();
+        for (int i = 0; i < keys.length; i++) {
+            ByteBuffer keyBuffer = ByteBuffer.wrap(keys[i]);
+            BoundStatement s = getStatement.bind(keyBuffer);
+            querySemaphore.acquire();
+            ResultSetFuture resultFuture = session.executeAsync(s);
+            Futures.addCallback(resultFuture, this.callback);
+            futures.add(resultFuture);
+        }
+        List<ResultSet> resultSets = futures.stream().map(f -> f.getUninterruptibly()).collect(Collectors.toList());
+        Iterator<ResultSet> iterator = resultSets.iterator();
+        for (int i = 0; i < keys.length; i++) {
+            ResultSet r = iterator.next();
+            assert r != null;
+            Row row = r.one();
+            if (row != null && !row.isNull(0)) {
+                result[i] = row.getBytes(0).array();
+            }
+        }
+        return result;
     }
 
     @Override
     public void put(byte[] key, byte[] value) throws Exception {
+        ByteBuffer keyBuffer = ByteBuffer.wrap(key);
+        ByteBuffer valueBuffer = ByteBuffer.wrap(value);
+        BoundStatement statement = putStatement.bind(keyBuffer, valueBuffer);
+        session.execute(statement);
+    }
 
+    public void putAll(byte[][] keys, byte[][] values) throws Exception {
+        List<ResultSetFuture> futures = new ArrayList<>();
+        for (int i = 0; i < keys.length; i++) {
+            ByteBuffer keyBuffer = ByteBuffer.wrap(keys[i]);
+            ByteBuffer valueBuffer = ByteBuffer.wrap(values[i]);
+            BoundStatement s = putStatement.bind(keyBuffer, valueBuffer);
+            querySemaphore.acquire();
+            ResultSetFuture future = this.session.executeAsync(s);
+            Futures.addCallback(future, this.callback);
+            futures.add(future);
+        }
+        futures.forEach(f -> f.getUninterruptibly());
     }
 
     @Override
     public void close() throws Exception {
-
+        session.close();
+        cluster.close();
     }
 
 
@@ -66,6 +125,19 @@ public class CassandraClient extends BasicKVDatabaseClient {
         this.keyColumnName = conf.getProperty(CONF_COLUMN_KEY, DEFAULT_COLUMN_KEY);
         this.valueColumnName = conf.getProperty(CONF_COLUMN_VALUE, DEFAULT_COLUMN_VALUE);
         this.useHashedKey = Boolean.parseBoolean(conf.getProperty(CONF_USE_HASHED_KEY, DEFAULT_USE_HASHED_KEY));
+        this.numConcurrentQuery = Integer.parseInt(conf.getProperty(CONF_CONCURRENT_QUERY, DEFAULT_CONCURRENT_QUERY));
+    }
+
+    private void initKeyspaceAndTable() throws Exception {
+        if (this.cluster.getMetadata().getKeyspace(this.keyspace) == null) {
+            this.session.execute("CREATE KEYSPACE " + this.keyspace + " WITH replication = {'class':'SimpleStrategy', 'replication_factor': 3}");
+            logger.info("Create the keyspace");
+        }
+        if (this.cluster.getMetadata().getKeyspace(this.keyspace).getTable(this.tableName) == null) {
+            createTable();
+            waitForTableCreation();
+            logger.info("Create the table");
+        }
     }
 
     @Override
@@ -79,7 +151,11 @@ public class CassandraClient extends BasicKVDatabaseClient {
         this.cluster = builder.build();
         this.session = this.cluster.connect();
         logger.info("Cassandra database connection established.");
-    }
+        initKeyspaceAndTable();
+        this.querySemaphore = new Semaphore(this.numConcurrentQuery);
+        this.callback = new FutureSemaphoreCallback();
+        prepareStatements();
+   }
 
     @Override
     public void clearDB() throws Exception {
@@ -88,16 +164,68 @@ public class CassandraClient extends BasicKVDatabaseClient {
             logger.info("Create the keyspace");
         }
         if (this.cluster.getMetadata().getKeyspace(this.keyspace).getTable(this.tableName) != null) {
-            logger.info("Drop the old table.");
             this.session.execute("DROP TABLE " + this.qulifiedTableName);
+            logger.info("Dropped the old table.");
         }
-        this.session.execute("CREATE TABLE " + this.qulifiedTableName +
-                "( " + this.keyColumnName + " blob PRIMARY KEY, " + this.valueColumnName + " blob)");
+        logger.info("Start re-create the table.");
+        createTable();
+        waitForTableCreation();
         logger.info("Table " + this.tableName + " re-created.");
+        logger.info("Now prepare statements.");
+        prepareStatements();
     }
 
     @Override
     public void createDB() throws Exception {
         clearDB();
+    }
+
+    private void prepareStatements() {
+        this.getStatement = this.session.prepare("SELECT " + this.valueColumnName
+                + " FROM " + this.qulifiedTableName
+                + " WHERE " + this.keyColumnName + " = ?");
+        this.putStatement = this.session.prepare("INSERT INTO " + this.qulifiedTableName
+                + " (" + this.keyColumnName + "," + this.valueColumnName + ") VALUES (?, ?)");
+    }
+
+    private void createTable() {
+        logger.info("Try to create the table.");
+        ResultSet r = this.session.execute("CREATE TABLE IF NOT EXISTS " + this.qulifiedTableName +
+                "(" + this.keyColumnName + " blob PRIMARY KEY, " + this.valueColumnName + " blob);");
+        logger.info(r.toString());
+    }
+
+    private void waitForTableCreation() throws Exception{
+        boolean flag = false;
+        int tryCount = 0;
+        while (!flag && tryCount < 20) {
+            if (this.cluster.getMetadata().getKeyspace(this.keyspace).getTable(this.tableName) == null) {
+                try {
+                    Thread.sleep(1000);
+                    tryCount++;
+                    System.err.print(".");
+                    createTable();
+                } catch (InterruptedException e) {
+                }
+            } else {
+                flag = true;
+            }
+        }
+        if (!flag && this.cluster.getMetadata().getKeyspace(this.keyspace).getTable(this.tableName) == null) {
+            throw new Exception("Cannot create table!");
+        }
+    }
+
+    private class FutureSemaphoreCallback implements FutureCallback<ResultSet> {
+
+        @Override
+        public void onSuccess(ResultSet rows) {
+            querySemaphore.release();
+        }
+
+        @Override
+        public void onFailure(Throwable throwable) {
+            querySemaphore.release();
+        }
     }
 }
